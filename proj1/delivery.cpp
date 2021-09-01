a6266ddd8a657c6d5916218fbaee75cced50ff42
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-08-31 23:58:01 -0800

    add: proj1 (initial files)

---
+++ proj1/delivery.cpp
@@ -0,0 +1,6 @@
+// delivery.cpp
+// Solomon Himelbloom
+// 2021-08-31
+// Source for class Delivery.
+
+#include "delivery.h"
---
+++ proj1/delivery.h
@@ -0,0 +1,9 @@
+// delivery.h
+// Solomon Himelbloom
+// 2021-08-31
+// Header for class Delivery.
+
+#ifndef PROJ1_DELIVERY_H
+#define PROJ1_DELIVERY_H
+
+#endif //PROJ1_DELIVERY_H
