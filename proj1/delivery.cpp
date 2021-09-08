d778fafa1421e31ce78212b8e3a1b7c9e8fff5c3
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-09-07 16:41:01 -0800

    fix: code style & readability issues (proj1)

--- proj1/delivery.cpp
+++ proj1/delivery.cpp
@@ -5,53 +5,70 @@
 
 #include "delivery.h"
 
+// Delivery::Delivery
 Delivery::Delivery() {
     setName("UNSPECIFIED");
     setQuantity(0);
     setMonth(1);
 }
 
+// Delivery::Delivery
 Delivery::Delivery(const std::string & prod, int num, int mm) {
     assert(mm <= 12 && mm >= 1 && num >= 0);
+
     setName(prod);
     setQuantity(num);
     setMonth(mm);
 }
 
+// Delivery::getName
 std::string Delivery::getName() const {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     return _product;
 }
 
+// Delivery::getQuantity
 int Delivery::getQuantity() const {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     return _quantity;
 }
 
+// Delivery::getMonth
 int Delivery::getMonth() const {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     return _month;
 }
 
+// Delivery::setName
 void Delivery::setName(std::string product) {
     _product = std::move(product);
 }
 
+// Delivery::setQuantity
 void Delivery::setQuantity(int quantity) {
     assert(_quantity >= 0);
+
     _quantity = quantity;
 }
 
+// Delivery::setMonth
 void Delivery::setMonth(int month) {
     assert(month <= 12 && month >= 1);
+
     _month = month;
 }
 
+// Delivery::empty
 bool Delivery::empty() const {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     return _quantity == 0;
 }
 
+// Delivery::toString
 std::string Delivery::toString() const {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
 
@@ -62,8 +79,8 @@ std::string Delivery::toString() const {
             "Oct", "Nov", "Dec"};
 
     std::string year = getName()
-            + " (" + std::to_string(getQuantity()) + "), "
-            + month[getMonth() - 1];
+                       + " (" + std::to_string(getQuantity()) + "), "
+                       + month[getMonth() - 1];
 
     return year;
 }
@@ -71,6 +88,7 @@ std::string Delivery::toString() const {
 // op++ [pre]
 Delivery & Delivery::operator++() {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     ++_quantity;
     return *this;
 }
@@ -78,6 +96,7 @@ Delivery & Delivery::operator++() {
 // op++ [post]
 Delivery Delivery::operator++([[maybe_unused]] int dummy) {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     auto save = *this;
     ++(*this);
     return save;
@@ -86,38 +105,45 @@ Delivery Delivery::operator++([[maybe_unused]] int dummy) {
 // op-- [pre]
 Delivery & Delivery::operator--() {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     if (_quantity > 0) {
         --_quantity;
     }
+
     return *this;
 }
 
 // op-- [post]
 Delivery Delivery::operator--([[maybe_unused]] int dummy) {
     assert(_month <= 12 && _month >= 1 && _quantity >= 0);
+
     auto save = *this;
     --(*this);
     return save;
 }
 
+// operator << (ostream, Delivery)
 std::ostream & operator<<(std::ostream & str, const Delivery & obj) {
     assert(obj.getMonth() <= 12 && obj.getMonth() >= 1 && obj.getQuantity() >= 0);
+
     str << obj.toString();
     return str;
 }
 
-// Equality (“==”) operator for comparing two Delivery objects.
+// Equality operator comparison
 bool operator==(const Delivery & lhs, const Delivery & rhs) {
     assert(rhs.getMonth() <= 12 && rhs.getMonth() >= 1 && rhs.getQuantity() >= 0);
     assert(lhs.getMonth() <= 12 && lhs.getMonth() >= 1 && lhs.getQuantity() >= 0);
+
     return lhs.getName() == rhs.getName()
-    && lhs.getQuantity() == rhs.getQuantity()
-    && lhs.getMonth() == rhs.getMonth();
+        && lhs.getQuantity() == rhs.getQuantity()
+        && lhs.getMonth() == rhs.getMonth();
 }
 
-// Inequality (“!=”) operator for comparing two Delivery objects.
+// Inequality operator comparison
 bool operator!=(const Delivery & lhs, const Delivery & rhs) {
     assert(rhs.getMonth() <= 12 && rhs.getMonth() >= 1 && rhs.getQuantity() >= 0);
     assert(lhs.getMonth() <= 12 && lhs.getMonth() >= 1 && lhs.getQuantity() >= 0);
+
     return !(lhs == rhs);
 }
