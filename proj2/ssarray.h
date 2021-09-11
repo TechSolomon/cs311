ebd3e1f5dfd61adf66ed396711f504216b050b9c
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-09-10 22:55:32 -0800

    add: proj2 (initial files)

---
+++ proj2/ssarray.h
@@ -0,0 +1,15 @@
+// ssarray.h
+// Solomon Himelbloom
+// 2021-09-10
+// Header for class template SSArray.
+
+#ifndef PROJ2_SSARRAY_H
+#define PROJ2_SSARRAY_H
+
+
+class SSArray {
+
+};
+
+
+#endif //PROJ2_SSARRAY_H
