2caa4beb2f9c997ea668835b07b2a23016264783
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-09-26 23:05:20 -0800

    add: proj4 (initial files)

---
+++ proj4/hdtcount.cpp
@@ -0,0 +1,6 @@
+// hdtcount.cpp
+// Solomon Himelbloom
+// 2021-09-26
+// Source for Holey Domino Tilings.
+
+#include "hdtcount.h"
---
+++ proj4/hdtcount.h
@@ -0,0 +1,9 @@
+// hdtcount.h
+// Solomon Himelbloom
+// 2021-09-26
+// Header for Holey Domino Tilings.
+
+#ifndef PROJ4_HDTCOUNT_H
+#define PROJ4_HDTCOUNT_H
+
+#endif //PROJ4_HDTCOUNT_H
