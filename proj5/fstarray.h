520429a953b618015ed98780e277f1a5ccd6ea59
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-10-21 23:37:39 -0800

    add: proj5 (initial files)

---
+++ proj5/fstarray.h
@@ -0,0 +1,9 @@
+// fstarray.h
+// Solomon Himelbloom
+// 2021-10-21
+// Class template for Project 5 (Frightfully Smart Array).
+
+#ifndef PROJ5_FSTARRAY_H
+#define PROJ5_FSTARRAY_H
+
+#endif //PROJ5_FSTARRAY_H
