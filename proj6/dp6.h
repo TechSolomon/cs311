5f527df6778d4b1083f6e32752b1a752f029599a
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-10-29 22:46:06 -0800

    add: proj6 (initial files)

---
+++ proj6/dp6.h
@@ -0,0 +1,24 @@
+// dp6.h
+// Benjamin Stream
+// Solomon Himelbloom
+// 2021-10-29
+// Function / class template for Project 6.
+
+#ifndef PROJ6_DP6_H
+#define PROJ6_DP6_H
+
+#include "llnode2.h"
+
+// Exercise A — Reversing a Linked List
+template<typename ValType>
+void reverseList(std::unique_ptr<LLNode2<ValType>> & head) {
+    // TODO: WRITE THIS!!!
+}
+
+// Exercise B — Associative Dataset Class Template
+template<typename KVType>
+class LLMap {
+    // TODO: WRITE THIS!!!
+};
+
+#endif //PROJ6_DP6_H
