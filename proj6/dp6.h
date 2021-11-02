cb4c32f86073dc861bf8ac7e326f6bf38f45e632
Benjamin Stream <brstream@alaska.edu>
2021-11-02 15:42:16 -0800

    BensBranchProj6 Skeletonized out Functions for exercize B

--- proj6/dp6.h
+++ proj6/dp6.h
@@ -16,11 +16,98 @@ void reverseList(std::unique_ptr<LLNode2<ValType>> & head) {
 }
 
 // Exercise B — Associative Dataset Class Template
+
+//Class LLMap
+//KVType must be std::pair or struct (or any appropriate key-value type)
+
 template<typename KVType>
 class LLMap {
 public:
 
+// ***** LLMAP: ctors, dctors, ops *****
 private:
+
+//Default Constructor
+//Strong Guarantee
+    explicit LLMap()
+        :_LLMapptr = unique_ptr<nullptr>;
+    {}
+
+
+//Deconstructor
+//No-Throw Guarantee
+    ~LLMap()
+    {
+        _LLMapptr.reset;
+    }
+
+// ***** LLMap: Data Member *****
+private:
+    auto _LLMapptr = unique_ptr<LLNode2<KVType>>;
+
+// ***** LLMap: Public Functions *****
+public:
+
+    //size
+    //TODO
+    int size() {
+        return 1;
+    }
+
+    //empty
+    //No-Throw Guarantee
+    bool empty() {
+        if (this->size() != 0) {
+            return false;
+        }
+        else return true;
+    }
+
+    //Non-Const Find
+    //??? Guarantee
+
+   auto find(auto DATA_TYPE) {
+        //if Data_type found
+        return DATA_TYPE*;
+        //else
+        //return nullptr
+    }
+
+    //Const Find
+    //??? Guarantee
+   auto const find(auto const DATA_TYPE) {
+        //if Data_type found
+        return const DATA_TYPE*;
+        //else not found
+        //return nullptr
+    }
+
+   //Insert
+   //??? Guarantee
+   void insert(auto KEY_TYPE, auto DATA_TYPE)
+   {
+       //goto keytype if exists
+       //set value to data_type
+       //else  _LLMapptr = unique_ptr<LLNode2<KVType>(KEY_TYPE,DATA_TYPE)>;
+   }
+
+   //Erase
+   //No-Throw Guarantee
+   void erase(auto KEY_TYPE)
+   {
+       //if keytype exists goto keytpe
+       // _LLMAPPtr = nullptr;
+       //else nothing
+   }
+
+   //Transverse
+   // ??? Guarantee
+   void transverse(auto someFunction(auto KEY_TYPE,auto DATA_TYPE))
+   {
+       //auto go through LLMAP
+       //someFunction() works on data
+       //end
+   }
 
 };
 
