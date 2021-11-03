534090d8961387b5500f9870605b92779a1400d6
Benjamin Stream <brstream@alaska.edu>
2021-11-02 21:33:13 -0800

    BensBranchProj6 More building of the functions

--- proj6/dp6.h
+++ proj6/dp6.h
@@ -17,98 +17,120 @@ void reverseList(std::unique_ptr<LLNode2<ValType>> & head) {
 
 // Exercise B — Associative Dataset Class Template
 
+
 //Class LLMap
 //KVType must be std::pair or struct (or any appropriate key-value type)
 
-template<typename KVType>
+
+template<typename Key, typename Val>
 class LLMap {
+
+    // key_type: type of key items
+    using key_type = Key;
+
+    // data_type: type of data items
+    using data_type = Val;
+
+   using KVType = std::pair<key_type, data_type>;
+
 public:
 
-// ***** LLMAP: ctors, dctors, ops *****
-private:
+    // ***** LLMAP: ctors, dctors, ops *****
 
-//Default Constructor
-//Strong Guarantee
-    explicit LLMap()
-        :_LLMapptr = unique_ptr<nullptr>;
-    {}
+    // Ctor
+    LLMap() : _data(nullptr) {
 
-
-//Deconstructor
-//No-Throw Guarantee
-    ~LLMap()
-    {
-        _LLMapptr.reset;
     }
 
-// ***** LLMap: Data Member *****
-private:
-    auto _LLMapptr = unique_ptr<LLNode2<KVType>>;
+    // Dctor
+    ~LLMap() = default;
 
-// ***** LLMap: Public Functions *****
+    // No other Big Five functions
+    LLMap(const LLMap& other) = delete;
+    LLMap& operator=(const LLMap& other) = delete;
+    LLMap(LLMap&& other) = delete;
+    LLMap& operator=(LLMap&& other) = delete;
+
+
+
+    // ***** LLMap: Public Functions *****
 public:
 
-    //size
-    //TODO
-    int size() {
-        return 1;
+    // size
+    [[nodiscard]] size_t size() const {
+        int counter = 0;
+        while (true) {
+            if(_data.)
+            ++counter;
+            
+        }
     }
 
     //empty
     //No-Throw Guarantee
-    bool empty() {
-        if (this->size() != 0) {
-            return false;
+    [[nodiscard]] bool empty() const {
+        if (!_data) //checks if _data (smartpointer) is null
+        {
+            return true;
         }
-        else return true;
+        else return false;
     }
 
     //Non-Const Find
     //??? Guarantee
-
-   auto find(auto DATA_TYPE) {
-        //if Data_type found
-        return DATA_TYPE*;
-        //else
-        //return nullptr
+    data_type* find(const key_type& key) {
+        //TODO: WRITE THIS!!!
+       //if Data_type found
+        return _data*;
+        //else not found
+        return nullptr;
     }
 
     //Const Find
     //??? Guarantee
-   auto const find(auto const DATA_TYPE) {
+    const data_type* find(const key_type& key) const {
+        //TODO: WRITE THIS!!!
         //if Data_type found
-        return const DATA_TYPE*;
+        return const _data*;
         //else not found
-        //return nullptr
+        return nullptr;
     }
 
    //Insert
    //??? Guarantee
-   void insert(auto KEY_TYPE, auto DATA_TYPE)
-   {
-       //goto keytype if exists
-       //set value to data_type
-       //else  _LLMapptr = unique_ptr<LLNode2<KVType>(KEY_TYPE,DATA_TYPE)>;
-   }
+    void insert(key_type key, data_type value) {
+        // TODO: WRITE THIS!!!
+        if (find(key) != nullptr)
+        {
+            
+        }
+        else
+        {
+            _data = unique_ptr<LLNode2<std::pair<key,value>>>;
+        }
+    }
 
-   //Erase
-   //No-Throw Guarantee
-   void erase(auto KEY_TYPE)
-   {
-       //if keytype exists goto keytpe
-       // _LLMAPPtr = nullptr;
-       //else nothing
-   }
+    //Erase
+    //No-Throw Guarantee
+    void erase(key_type key) {
+        if (find(key)) {
 
-   //Transverse
-   // ??? Guarantee
-   void transverse(auto someFunction(auto KEY_TYPE,auto DATA_TYPE))
-   {
-       //auto go through LLMAP
-       //someFunction() works on data
-       //end
-   }
+        }
+    }
 
+    //Transverse
+    // ??? Guarantee
+    void traverse(const std::function<void(key_type, data_type)> example) const {
+        // TODO: WRITE THIS!!!
+        //auto go through LLMAP
+        //someFunction() works on data
+        //end
+    }
+
+
+    // ***** LLMap: Data Member *****
+private:
+    std::unique_ptr<LLNode2<KVType>> _data;
 };
 
 #endif //PROJ6_DP6_H
