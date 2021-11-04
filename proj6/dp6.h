c4adcaa53559bd12e20eece411ce6737751afd81
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-11-04 14:20:37 -0800

    add: comments for exception safety, type requirements, & invariants

--- proj6/dp6.h
+++ proj6/dp6.h
@@ -2,7 +2,7 @@
 // Benjamin Stream
 // Solomon Himelbloom
 // 2021-10-29
-// Function / class template for Project 6.
+// Class template for Project 6.
 
 #ifndef PROJ6_DP6_H
 #define PROJ6_DP6_H
@@ -13,25 +13,28 @@
 #include <utility> // For std::move, std::pair
 #include <functional> // For std::function
 
-// Exercise A — Reversing a Linked List
+// Exercise A — Reversing a Linked List (reverseList)
 // - newHead will point to this new list, which holds nodes that have been reversed.
 // - head points to the nodes that are not reversed yet.
-// - To finish, set head = newHead (unique_ptr).
+// - To finish, set head = newHead.
+// Requirements on Types:
+// - None.
+// No-Throw Guarantee
 template<typename ValType>
-void reverseList(std::unique_ptr<LLNode2<ValType>> &head) {
-
+void reverseList(std::unique_ptr<LLNode2<ValType>> &head) noexcept {
     std::unique_ptr<LLNode2<ValType>> newHead;
-
     while (head) {
         newHead.swap(head->_next);
         head.swap(newHead);
     }
-
     head = std::move(newHead);
 }
 
 // Exercise B — Associative Dataset Class Template (LLMap)
-// pair_type must be std::pair or struct (or any appropriate key-value type)
+// Invariants:
+// - Nodes must have unique keys.
+// Requirements on Types:
+// - key_type must have op==
 template<typename Key, typename Val>
 class LLMap {
 
@@ -41,7 +44,7 @@ class LLMap {
     // data_type: type of data items
     using data_type = Val;
 
-    // pair_type: pair of specified key_type & data_type items
+    // pair_type: a pair of specified key_type & data_type items
     using pair_type = std::pair<key_type, data_type>;
 
 // ***** LLMap: Data Member *****
@@ -69,50 +72,53 @@ public:
 
     // size
     // No-Throw Guarantee
+    // - Returns size of Linked List node(s).
     [[nodiscard]] size_t size() const {
         return ::size(_ptrNode);
     }
 
     // empty
     // No-Throw Guarantee
+    // - checks if _data (smart pointer) is null.
     [[nodiscard]] bool empty() const {
-        if (_ptrNode) return false; // checks if _data (smart pointer) is null
+        if (_ptrNode) return false;
         else return true;
     }
 
     // Non-Const Find
     // No-Throw Guarantee
-    // FIXME: Returns pointer to the pair.second;
+    // - Returns pointer to the pair.second;
     data_type* find(const key_type& key) {
         LLNode2<pair_type>* ptr = _ptrNode.get();
         while (ptr != nullptr) {
-            if (ptr->_data.first == key) return  &ptr->_data.second;
+            if (ptr->_data.first == key) return &ptr->_data.second;
             else ptr = ptr->_next.get();
         }
         return nullptr;
     }
 
     // Const Find
-    // TODO: No-Throw Guarantee
+    // No-Throw Guarantee
+    // - Returns const pointer to the pair.second;
     const data_type* find(const key_type& key) const {
         LLNode2<pair_type>* ptr = _ptrNode.get();
         while (ptr != nullptr) {
-            if (ptr->_data.first == key) return  &ptr->_data.second;
+            if (ptr->_data.first == key) return &ptr->_data.second;
             else ptr = ptr->_next.get();
         }
         return nullptr;
     }
 
    // Insert
-   // TODO: ??? Guarantee
+   // Strong Guarantee
     void insert(const key_type key, data_type value) {
-        data_type * dummyPtr = find(key);
+        data_type *dummyPtr = find(key);
         if (dummyPtr != nullptr) dummyPtr = &value;
         else push_front(_ptrNode,pair_type{key,value});
     }
 
     // Erase
-    // No-Throw Guarantee
+    // Strong Guarantee
     void erase(const key_type key) {
         auto ptr = _ptrNode.get();
         auto prevPtr = &_ptrNode;
@@ -127,15 +133,11 @@ public:
     }
 
     // Traverse
-    // TODO: ??? Guarantee
+    // Basic Guarantee (example unknown)
     void traverse(const std::function<void(key_type, data_type)> example) const {
         auto ptr = _ptrNode.get();
-        key_type *key;
-        data_type *value;
         while (ptr != nullptr) {
-            key = &ptr->_data.first;
-            value = &ptr->_data.second;
-            example(*key, *value);
+            example(ptr->_data.first, ptr->_data.second);
             ptr = ptr->_next.get();
         }
     }
