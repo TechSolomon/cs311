96fc9bd76f18a6a8fb776330cb2696512bfe35ab
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-11-15 19:13:58 -0900

    add: author information & failing checks

--- proj7/treesorter.h
+++ proj7/treesorter.h
@@ -1,5 +1,6 @@
-// treesorter.h  SKELETON
-// Glenn G. Chappell
+// treesorter.h
+// Solomon Himelbloom
+// Benjamin Stream
 // 2021-11-08
 //
 // For CS 311 Fall 2021
@@ -11,18 +12,6 @@
 
 #include <iterator>
 // For std::iterator_traits
-
-// *******************************************************
-// * YOU MIGHT WANT TO GET RID OF THE FOLLOWING INCLUDES *
-// *******************************************************
-
-#include <algorithm>
-// For std::stable_sort, std::move
-#include <vector>
-// For std::vector
-#include <iterator>
-// For std::distance
-
 
 // treesort
 // Sort a given range using Treesort.
@@ -37,13 +26,6 @@ void treesort(FDIter first, FDIter last)
 {
     // Value is the type that FDIter points to
     using Value = typename std::iterator_traits<FDIter>::value_type;
-
-    // THE FOLLOWING IS DUMMY CODE. IT WILL PASS ALL TESTS, BUT IT DOES
-    // NOT MEET THE REQUIREMENTS OF THE PROJECT.
-    std::vector<Value> buff(std::distance(first, last));
-    std::move(first, last, buff.begin());
-    std::stable_sort(buff.begin(), buff.end());
-    std::move(buff.begin(), buff.end(), first);
 }
 
 
