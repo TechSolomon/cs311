c103f55a5fbe569c29c900a7a2504715efb492f1
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-11-21 15:50:10 -0900

    add: proj8 (initial files)

---
+++ proj8/countwords.cpp
@@ -0,0 +1,12 @@
+// countwords.cpp
+// Benjamin Stream
+// Solomon Himelbloom
+// 2021-11-21
+// Source for Exercise A — Program: Counting Words.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Counting Words." << std::endl;
+    return 0;
+}
