4e1d0678853aec7d47eb23727289dd57ed629284
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-11-29 16:37:43 -0900

    add: userPause & code comments (countwords)

--- proj8/countwords.cpp
+++ proj8/countwords.cpp
@@ -6,7 +6,33 @@
 
 #include <iostream>
 
-int main() {
+// TODO: Store results (before printing) in an appropriate STL container.
+
+void count() {
     std::cout << "Hello, Counting Words." << std::endl;
-    return 0;
+}
+
+// userPause
+// Wait for user to press ENTER: read all chars through first newline.
+void userPause() {
+    std::cout.flush();
+    while (std::cin.get() != '\n');
+}
+
+int main() {
+    count();
+
+    // FIXME: Edge cases to avoid all crashes:
+    //  - For example, what if the user, when prompted for a filename, just hits ENTER?
+    //  - What if they type a bunch of blanks and then hit ENTER?
+    //  - What about a filename with a blank in the middle?
+    //  - What about the name of a file that does not exist?
+    //  - A file that exists but cannot be read?
+    //  - An empty file?
+    //  - An executable file? a directory?
+    //  - Etc.
+
+    // Wait for user
+    std::cout << "Press ENTER to quit ";
+    userPause();
 }
